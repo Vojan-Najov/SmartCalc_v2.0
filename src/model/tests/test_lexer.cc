9180f7d7235308a38ee008158ec01cb16ede6fde
Vojan-Najov <serejah.mashichev@gmail.com>
2023-11-08 17:52:39 +0300

    Fix tests for lexer

--- src/model/tests/test_lexer.cc
+++ src/model/tests/test_lexer.cc
@@ -7,60 +7,94 @@ class LexerTest : public testing::Test {
 };
 
 TEST_F(LexerTest, EmptyString) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected;
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"", errmsg};
+	const char *expected[] = {};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, SpacesString) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("   \t   \n   \v    ");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected;
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"    \t     \n   ", errmsg};
+	const char *expected[] = {};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String1) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("   11  ");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"11"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"    11       ", errmsg};
+	const char *expected[] = {"11"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String2) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("1 2");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"12"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"   1   2   ", errmsg};
+	const char *expected[] = {"1", "2"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String3) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("1 + 2");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"1+2"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"1+2", errmsg};
+	const char *expected[] = {"1", "+", "2"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String4) {
-	std::list<std::string> lexems = s21::lexer::str_to_lexems("-1 + 2mod3");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"-1+2%3"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"-1 + 2mod3", errmsg};
+	const char *expected[] = {"-", "1", "+", "2", "%", "3"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String5) {
-	std::list<std::string> lexems = 
-		s21::lexer::str_to_lexems("-1+2*4/(17+sin(10))/14mod3");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"-1+2*4/(17+sin(10))/14%3"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"-1+2*4/(17+sin(10))/14mod3", errmsg};
+	const char *expected[] = {"-", "1", "+", "2", "*", "4", "/",
+                            "(", "17", "+", "sin", "(", "10", 
+                            ")", ")", "/", "14", "%", "3"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 
 TEST_F(LexerTest, String6) {
-	std::list<std::string> lexems = 
-		s21::lexer::str_to_lexems("-1 + +2 * - 4 /ln(17+sin(10))/14mod3");
-	std::string dump = s21::lexer::lexems_to_string(lexems);
-	std::string expected{"-1++2*-4/ln(17+sin(10))/14%3"};
-	EXPECT_EQ(dump, expected);
+	std::string errmsg;
+	s21::Lexer lexer{"-1 + +2 * -4 / ln(17+sin(10))/14mod3", errmsg};
+	const char *expected[] = {"-", "1", "+", "+", "2", "*", "-",
+														"4", "/", "ln", "(", "17", "+", "sin", "(", "10", 
+                            ")", ")", "/", "14", "%", "3"};
+
+	for (size_t i = 0; i < sizeof(expected) / sizeof(const char *); ++i) {
+		EXPECT_EQ(lexer.next(), std::string{expected[i]});
+	}
+	EXPECT_TRUE(lexer.empty());
 }
 

74ec1daa1dc5ecb741b2ac03e120ba459b2df945
Vojan-Najov <serejah.mashichev@gmail.com>
2023-11-20 23:00:15 +0300

    Add smartcalc header

---
+++ src/libsmartcalc/smartcalc.h
@@ -0,0 +1,15 @@
+#ifndef LIBSMARTCALC_SMARTCALC_H_
+#define LIBSMARTCALC_SMARTCALC_H_
+
+namespace s21 {
+
+class Smartcalc final {
+	double CalculateExpr(const char *expr);
+	// ErrorStatus set_variable(const char *name, const char *expr);
+	// ErrorStatus SetFunction(const char *name, const char *def);
+
+};
+
+}
+
+#endif  // LIBSMARTCALC_SMARTCALC_H_

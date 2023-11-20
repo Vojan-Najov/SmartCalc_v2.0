a2659919b0149bd5ad1e7780e00c05a073b03a00
Vojan-Najov <serejah.mashichev@gmail.com>
2023-11-20 23:01:53 +0300

    Add model header

---
+++ src/model/model.h
@@ -0,0 +1,24 @@
+#ifndef MODEL_MODEL_H_
+#define MODEL_MODEL_H_
+
+#include <string>
+
+namespace s21 {
+
+class Model final {
+ public:
+	// std::string calculate_expr(const char *str);
+	// void calculate_credit(void);
+	// void calculate_debit(void);
+
+ private:
+	Smartcalc sc_;
+	// Debitcalc dc_;
+	// Creditcalc cc_;
+};
+
+}
+
+}  // namespace s21
+
+#endif  // MODEL_MODEL_H_

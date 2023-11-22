5e2114df1598b8b3f5bf7265f88e7d17fed8ee94
Vojan-Najov <serejah.mashichev@gmail.com>
2023-11-22 22:09:44 +0300

    Add tests for rpn

---
+++ src/libsmartcalc/tests/test_rpn.cc
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "rpn.h"
+#include "operations.h"
+
+using Rpn = s21::smartcalc::Rpn;
+using AToken = s21::smartcalc::AToken;
+using NumberToken = s21::smartcalc::NumberToken;
+using UnaryOpToken = s21::smartcalc::UnaryOpToken;
+using BinaryOpToken = s21::smartcalc::BinaryOpToken;
+using FuncToken = s21::smartcalc::FuncToken;
+using WrongToken = s21::smartcalc::WrongToken;
+using TokenType = s21::smartcalc::TokenType;
+
+class RpnTest : public testing::Test {
+
+};
+
+TEST_F(RpnTest, Expression01) {
+	Rpn rpn;
+
+	rpn.Push(new NumberToken{15.0});
+	rpn.Push(new NumberToken{10.0});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::sub});
+	rpn.Calculate();
+	EXPECT_EQ(rpn.Result(), 5.0);
+}
+
+TEST_F(RpnTest, Expression02) {
+	Rpn rpn;
+
+	// 3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2 ^ 3
+	double result = 3.0 + 4.0 * 2.0 / std::pow((1.0 - 5.0), pow(2.0, 3.0));
+
+	// 3 4 2 × 1 5 − 2 3 ^ ^ ÷ +
+	rpn.Push(new NumberToken{3.0});
+	rpn.Push(new NumberToken{4.0});
+	rpn.Push(new NumberToken{2.0});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::multiply});
+	rpn.Push(new NumberToken{1.0});
+	rpn.Push(new NumberToken{5.0});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::sub});
+	rpn.Push(new NumberToken{2.0});
+	rpn.Push(new NumberToken{3.0});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::power});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::power});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::devide});
+	rpn.Push(new BinaryOpToken{&s21::smartcalc::binary_ops::sum});
+	rpn.Calculate();
+	EXPECT_EQ(rpn.Result(), result);
+}
+

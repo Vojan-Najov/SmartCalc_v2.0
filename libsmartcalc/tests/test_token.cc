a7f555c2f75c04d121862b7cff5463b223d9b51e
ccartman <ccartman@student.21-school.ru>
2023-12-21 22:13:36 +0300

    Add test for tokens

--- libsmartcalc/tests/test_token.cc
+++ libsmartcalc/tests/test_token.cc
@@ -10,6 +10,10 @@ using WrongToken = s21::smartcalc::WrongToken;
 using NumberToken = s21::smartcalc::NumberToken;
 using UnaryOpToken = s21::smartcalc::UnaryOpToken;
 using BinaryOpToken = s21::smartcalc::BinaryOpToken;
+using FuncToken = s21::smartcalc::FuncToken;
+using LeftBracketToken = s21::smartcalc::LeftBracketToken;
+using RightBracketToken = s21::smartcalc::RightBracketToken;
+using NameToken = s21::smartcalc::NameToken;
 
 class TokenTest : public testing::Test {};
 
@@ -46,4 +50,66 @@ TEST_F(TokenTest, BinaryOpToken) {
   BinaryOpToken *ct = t.clone();
   EXPECT_EQ(ct->dump(), "^");
   delete ct;
+}
+
+TEST_F(TokenTest, FuncToken) {
+  FuncToken fsin{&s21::smartcalc::funcs::sin};
+  EXPECT_EQ(fsin.dump(), "sin");
+
+  FuncToken fcos{&s21::smartcalc::funcs::cos};
+  EXPECT_EQ(fcos.dump(), "cos");
+
+  FuncToken ftan{&s21::smartcalc::funcs::tan};
+  EXPECT_EQ(ftan.dump(), "tan");
+
+  FuncToken fasin{&s21::smartcalc::funcs::asin};
+  EXPECT_EQ(fasin.dump(), "asin");
+
+  FuncToken facos{&s21::smartcalc::funcs::acos};
+  EXPECT_EQ(facos.dump(), "acos");
+
+  FuncToken fatan{&s21::smartcalc::funcs::atan};
+  EXPECT_EQ(fatan.dump(), "atan");
+
+  FuncToken fln{&s21::smartcalc::funcs::ln};
+  EXPECT_EQ(fln.dump(), "ln");
+
+  FuncToken flog{&s21::smartcalc::funcs::log};
+  EXPECT_EQ(flog.dump(), "log");
+
+  FuncToken fsqrt{&s21::smartcalc::funcs::sqrt};
+  EXPECT_EQ(fsqrt.dump(), "sqrt");
+}
+
+TEST_F(TokenTest, LeftBracketOpToken) {
+  LeftBracketToken t{};
+  LeftBracketToken *ct = t.clone();
+  EXPECT_EQ(ct->dump(), "(");
+  delete ct;
+}
+
+TEST_F(TokenTest, RightBracketOpToken) {
+  RightBracketToken t{};
+  RightBracketToken *ct = t.clone();
+  EXPECT_EQ(ct->dump(), ")");
+  delete ct;
+}
+
+TEST_F(TokenTest, NameOpToken) {
+  NameToken t{std::string{"tmp"}};
+  NameToken *ct = t.clone();
+  EXPECT_EQ(ct->dump(), "tmp");
+  delete ct;
+}
+
+TEST_F(TokenTest, OutputTest) {
+  std::cerr << s21::smartcalc::TokenType::Empty
+            << s21::smartcalc::TokenType::Wrong
+            << s21::smartcalc::TokenType::Number
+            << s21::smartcalc::TokenType::UnaryOp
+            << s21::smartcalc::TokenType::BinaryOp
+            << s21::smartcalc::TokenType::Function
+            << s21::smartcalc::TokenType::LeftBracket
+            << s21::smartcalc::TokenType::RightBracket
+            << s21::smartcalc::TokenType::Name << std::endl;
 }
